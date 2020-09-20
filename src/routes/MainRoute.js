0b23959b8d909881b808d63873caed752604e35e
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-20 15:07:53 +0700

    Push the first example

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,11 +1,17 @@
+//Packages
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WatchPage from "../pages/app/WatchPage";
+
+
+
+
 const MainRoute = () => {
     return (
       <Router>
         <Switch>
           <Route path="/" component={WatchPage} />
+          
         </Switch>
       </Router>
     );
