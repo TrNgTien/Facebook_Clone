327c4ef7d9a8f5589a841b9b7b885ce05958a425
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-20 14:29:15 +0700

    Add the core base

---
+++ src/pages/app/WatchPage.js
@@ -0,0 +1,12 @@
+import "../styles/WatchPageStyles.css";
+import React from "react";
+
+const WatchPage = ()=>{
+    return(
+        <div>
+            hi
+        </div>
+
+    );
+}
+export default WatchPage;
---
+++ src/routes/MainRoute.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import WatchPage from "../pages/app/WatchPage";
+const MainRoute = () => {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={WatchPage} />
+        </Switch>
+      </Router>
+    );
+  };
+  
+  export default MainRoute; 
