4afe6b1d9bcceed8a1640e7e0592dd546f537944
TrNgTien <ngoctien27122001@gmail.com>
2022-02-08 20:19:48 +0700

    Remove redundant code lines

--- client/src/app/pages/chatting/GeneralChat.tsx
+++ client/src/app/pages/chatting/GeneralChat.tsx
@@ -1,32 +1,11 @@
 import React, { useState } from "react";
+import NavigationBar from "../../components/sidebar-navigation/NavigationBar";
 import "./styles/GeneralChat.scss";
 const GeneralChat = () => {
-  const [counter, setCounter] = useState<number>(0);
+  // const [avatarImg, setAvatarImg] = useState<number>(0);
   return (
-    <div>
-      <div style={{ display: "flex", marginLeft: "45%", marginTop: "10%" }}>
-        <button
-          style={{
-            marginRight: "10px",
-            width: "100px",
-            fontSize: "2rem",
-            height: "50px",
-          }}
-          onClick={() => setCounter(counter - 1)}
-        >
-          -
-        </button>
-        <p style={{ marginRight: "10px", fontSize: "2rem" }}>{counter}</p>
-        <button
-          style={{ width: "100px", height: "50px", fontSize: "2rem" }}
-          onClick={() => setCounter(counter + 1)}
-        >
-          +
-        </button>
-      </div>
-      <p style={{ marginLeft: "50%", marginTop: "10%", fontSize: "2rem" }}>
-        State {counter}
-      </p>
+    <div className="general-chat__wapper">
+      <NavigationBar/>
     </div>
   );
 };
