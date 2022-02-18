5841872d73d53605320f9be75d399a75cb5f1099
TrNgTien <ngoctien27122001@gmail.com>
2022-02-18 19:18:46 +0700

    Adjust profilePage

--- client/src/app/pages/profile/ProfilePage.tsx
+++ client/src/app/pages/profile/ProfilePage.tsx
@@ -2,30 +2,30 @@ import React, { ChangeEvent, useState } from "react";
 import NavigationBar from "../../components/sidebar-navigation/NavigationBar";
 import { getCookie } from "../../utils/CookieUtil";
 import "./styles/ProfilePage.scss";
-
 export default function ProfilePage(props: any) {
   const userNameCookie = getCookie("userName");
   const imageUrlCookie = getCookie("imageUrl");
   const [displayName, setDisplayName] = useState<string>(userNameCookie);
-  const [displayAge, setDisplayAge] = useState<string>("18");
+  const [displayBirthday, setDisplayBirthday] = useState<string>("");
   const [displayHobbies, setDisplayHobbies] = useState<string>("");
   const [isEdit, setIsEdit] = useState<boolean>(false);
 
   const handleEditName = (e: ChangeEvent<HTMLInputElement>) => {
     setDisplayName(e.target.value);
   };
-  const handleEditAge = (e: ChangeEvent<HTMLInputElement>) => {
-    const convertedValue = Number(e.target.value);
-    if (convertedValue >= 18 && typeof convertedValue === "number") {
-      setDisplayAge(e.target.value);
-    }
+  const handleEditBirthday = (e: ChangeEvent<HTMLInputElement>) => {
+    setDisplayBirthday(e.target.value);
   };
   const handleEditHobbies = (e: ChangeEvent<HTMLInputElement>) => {
     setDisplayHobbies(e.target.value);
   };
   return (
     <div className="profile-page">
-      <NavigationBar imageUrl={imageUrlCookie} name={userNameCookie} history={props.history} />
+      <NavigationBar
+        imageUrl={imageUrlCookie}
+        name={userNameCookie}
+        history={props.history}
+      />
       <div className="profile-zone">
         <h2 className="title-page">Your Profile</h2>
         <div className="grid profile-zone">
@@ -34,16 +34,12 @@ export default function ProfilePage(props: any) {
               <div className="col l-4">
                 <img src={imageUrlCookie} alt="img" className="img-profile" />
                 {isEdit ? (
-                  <div className="row">
-                    <div className="col l-6">
-                      <button
-                        className="confirm-edit__btn"
-                        onClick={() => setIsEdit(false)}
-                      >
-                        Confirm
-                      </button>
-                    </div>
-                  </div>
+                  <button
+                    className="confirm-edit__btn"
+                    onClick={() => setIsEdit(false)}
+                  >
+                    Confirm
+                  </button>
                 ) : (
                   <button
                     className="edit-profile__btn"
@@ -57,10 +53,13 @@ export default function ProfilePage(props: any) {
             <div className="col l-2">
               <form>
                 <div className="listing-information">
-                  <p className="header-info">Your Display Name</p>
+                  <label className="header-info" htmlFor="fullnameOfUser">
+                    Your Display Name
+                  </label>
                   <input
                     disabled={isEdit ? false : true}
                     value={displayName}
+                    id="fullnameOfUser"
                     autoComplete="off"
                     onChange={handleEditName}
                     className={
@@ -69,28 +68,32 @@ export default function ProfilePage(props: any) {
                   />
                 </div>
                 <div className="listing-information">
-                  <p className="header-info">Your Age</p>
+                  <label className="header-info" htmlFor="birthday">
+                    Your Birthday
+                  </label>
                   <input
-                    type="range"
-                    value={displayAge}
+                    type="date"
+                    value={displayBirthday}
                     disabled={isEdit ? false : true}
                     autoComplete="off"
                     step={1}
-                    onChange={handleEditAge}
-                    name="volume"
-                    min={18}
-                    max={100}
+                    onChange={handleEditBirthday}
+                    id="birthday"
+                    min="1980-01-01"
+                    max={new Date().toISOString().split("T")[0]}
                     className={
                       isEdit ? "input-profile" : "input-profile--disabled "
                     }
                   />
-                  <p>{displayAge}</p>
                 </div>
                 <div className="listing-information">
-                  <p className="header-info">Your Hobbies</p>
+                  <label className="header-info" htmlFor="hobbies">
+                    Your Hobbies
+                  </label>
                   <input
                     disabled={isEdit ? false : true}
                     autoComplete="off"
+                    id="hobbies"
                     onChange={handleEditHobbies}
                     value={displayHobbies}
                     className={
