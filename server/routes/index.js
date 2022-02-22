c6b8205a9ca10d4b264245e8884c29171e040f3d
nguyenquocviet2007 <vietnguyen.170715@gmail.com>
2022-02-22 15:52:03 +0700

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ const socialFeedRoute = require("./SocialFeedRoute.js");
 const FEED = require("../constant/ConstantRoutes");
 function routes(app) {
   app.use('/feed', socialFeedRoute);
+  app.use('/feedsss', socialFeedRoute);
 }
 module.exports = routes;
