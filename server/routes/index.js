fd84608f22be002eff0e13c85da6e339be8bbcb4
TrNgTien <ngoctien27122001@gmail.com>
2022-02-22 15:52:17 +0700

    test conflicted

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,7 @@ const socialFeedRoute = require("./SocialFeedRoute.js");
 const FEED = require("../constant/ConstantRoutes");
 function routes(app) {
   app.use('/feed', socialFeedRoute);
+  app.use('/feedss', socialFeedRoute);
+
 }
 module.exports = routes;
