b8bada01c4481f46be1a905990de91872a212f95
TrNgTien <ngoctien27122001@gmail.com>
2022-02-22 16:01:12 +0700

    Remove redundant

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,5 @@ const socialFeedRoute = require("./SocialFeedRoute.js");
 const FEED = require("../constant/ConstantRoutes");
 function routes(app) {
   app.use('/feed', socialFeedRoute);
-  app.use('/feedsss', socialFeedRoute);
 }
 module.exports = routes;
