d535d577ca1b03cc09679a6ddfa4387580971b36
TrNgTien <ngoctien27122001@gmail.com>
2022-03-14 00:19:02 +0700

    Add file prettier and set-up redux-tookit and slices

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store/store";
+import { Provider } from "react-redux";
 import "./index.scss";
 import MainRoutes from "./app/routes/MainRoutes";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MainRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <MainRoutes />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
