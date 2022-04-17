6ddbed2a12f201b5634213e9b213cf69f98a058a
nguyenquocviet2007 <vietnguyen.170715@gmail.com>
2022-04-17 18:12:57 +0700

    route for matching

--- server/API/src/routes/index.js
+++ server/API/src/routes/index.js
@@ -1,8 +1,10 @@
 const socialFeedRoute = require("./SocialFeedRoute.js");
 const userRoute = require("./UserRoute");
+const mathRoute = require("./MathRoute");
 const route = require("../constant/ConstantRoutes");
 function routes(app) {
   app.use(route.FEED, socialFeedRoute);
   app.use(route.AUTH, userRoute);
+  app.use(route.MATCH, mathRoute);
 }
 module.exports = routes;
