b1ffc196f026aace0d38d0d57fc86d7dae002f19
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-04-20 23:51:05 +0700

    fixed notfound classname

--- client/src/app/pages/not-found/NotFound.tsx
+++ client/src/app/pages/not-found/NotFound.tsx
@@ -5,7 +5,7 @@ import "./NotFound.scss";
 function NotFound() {
   const NOTFOUND_IMG = "https://images.ui8.net/uploads/6_1632688928415.png";
   return (
-    <div className='container-notfound'>
+    <div className='container-not-found'>
       <img className='notfound-img' src={NOTFOUND_IMG} alt='' />
       <div className='container-content'>
         <h1 className='notfound-title'>Oops</h1>
