69efbc6b60ade939f8c9ab3035ab07b91ff49181
nguyenquocviet2007 <vietnguyen.170715@gmail.com>
2022-06-06 23:10:05 +0700

    fix mistake

--- server/src/routes/ProfileRoute.js
+++ server/src/routes/ProfileRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userHandler = require("../controllers/UserHandler");
 const { Authentication } = require("../middleware/Authentication");
 
-router.put("/updateInfor/:id", Authentication, userHandler.updateInformation);
+router.put("/updateInfor/:userID", Authentication, userHandler.updateInformation);
 router.put(
   "/updateAvatar/:id",
   Authentication,
