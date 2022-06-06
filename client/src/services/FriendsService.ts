65918cd1d5c3de992a013fd242345155e77c5993
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-06-07 01:30:22 +0700

    complete find friend page and add friend func

---
+++ client/src/services/FriendsService.ts
@@ -0,0 +1,30 @@
+import { axiosInstance } from "./BaseService";
+import API_PATH from "@constants/API_PATH";
+
+interface IAddFriend {
+  token: string | null;
+  friendId: string;
+}
+
+const getAllUser = (token: string | null) => {
+  let configHeader = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return axiosInstance.get(API_PATH.GET_ALL_USER, configHeader);
+};
+const addFriend = (reqBody: IAddFriend) => {
+  const { token, friendId } = reqBody;
+
+  let configHeader = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  console.log(configHeader);
+
+  return axiosInstance.put(`${API_PATH.ADD_FRIEND}/${friendId}`, {}, configHeader);
+};
+
+export { getAllUser, addFriend };
